0622a0978f83a509bb7a47748031f802124f9fdc
hanwell2002 <hanwell2002@gmail.com>
2023-01-02 21:18:17 -0500

    updates

--- postgres-jdbc-access/src/main/scala/Main.scala
+++ postgres-jdbc-access/src/main/scala/Main.scala
@@ -6,16 +6,19 @@ import java.io.FileWriter
 import scala.collection.mutable.ListBuffer
 
 object Main {
+  val csvFile = "C:/var/output/resultset.csv"
+
   def main(args: Array[String]): Unit = {
     databaseAccessTest()
+
     csvTest()
   }
 
   def databaseAccessTest() {
     // connect to the database
+    /*
     val driver = "org.postgresql.Driver"
-    val url = "jdbc:postgresql://localhost:5432/postgres"
-    val con_str = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=MY_PASSWORD"
+    val con_str = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=MY_DB_PASSWORD"
 
     val conn = DriverManager.getConnection(con_str)
     try {
@@ -27,28 +30,43 @@ object Main {
     } finally {
       conn.close()
     }
+    */
+
     // Class.forName(driver)
-    var connection: Connection = null
-    val query = "SELECT * from city"
+    val url = "jdbc:postgresql://localhost:5432/postgres"
     val username = "postgres"
-    val password = "MY_PASSWORD
+    val password = "MY_DB_PASSWORD"
+    var connection: Connection = null
+    val query = "SELECT * from country"
+
     try {
       // classOf[org.postgresql.Driver]
       connection = DriverManager.getConnection(url, username, password)
-
-      // create the statement, and run the select query
+      // create the statement, and run the query
       import java.sql.ResultSet
       val statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
-      val resultSet = statement.executeQuery("SELECT * from country")
+      val resultSet = statement.executeQuery(query)
 
-      while (resultSet.next) {
-        val countryName = resultSet.getString("code")
-        val location = resultSet.getString("region")
-        println("country-name = %s, location = %s".format(countryName, location))
-      }
+      /*   while (resultSet.next) {
+           val countryName = resultSet.getString("code")
+           val location = resultSet.getString("region")
+           println("country-name = %s, location = %s".format(countryName, location))
+         }
+     */
+      println("###################################################")
+
+      ReadCsvUtil.writeAll(resultSet, csvFile, true)
+
+      println("###################################################")
+      // check if csv file generated
+      checkCsvFile()
+
+      // add  first column of the table to list
+      // val list = ReadCsvUtil.getResult(resultSet)
+      println("-----------------------completed  ------------------------")
 
     } catch {
-      case e => e.printStackTrace
+          case e => e.printStackTrace
     }
     connection.close()
 
@@ -58,19 +76,31 @@ object Main {
      * val url: String = "jdbc:postgresql://localhost/postgres"
      * val props: Nothing = new Nothing
      * props.setProperty("user", "postgres")
-     * props.setProperty("password", "Admin$777")
+     * props.setProperty("password", "MY_PASSWORD")
      * props.setProperty("ssl", "true")
      * val conn: Connection = DriverManager.getConnection(url, props)
      *
-     * val url: String = "jdbc:postgresql://localhost/postgres?user=postgres&password=Admin$777&ssl=true"
+     * val url: String = "jdbc:postgresql://localhost/postgres?user=postgres&password=MY_DB_PASSWORD&ssl=true"
      * val conn: Connection = DriverManager.getConnection(url)
      */
   }
 
-  def csvTest():
+  private def csvTest():
   Unit = {
-    ReadCsvUtil.read()
-    ReadCsvUtil.write()
+    val csvDemoFile ="C:/var/output/demo-csv-write-read.csv"
+    ReadCsvUtil.write(csvDemoFile)
+    ReadCsvUtil.read(csvDemoFile)
   }
+
+  private def checkCsvFile(): Unit = {
+    println("Reading CSV ... ")
+    val reader = CSVReader.open(csvFile)
+    reader.readNext()
+    // reader.all()
+    reader.foreach(fields => println(fields))
+    reader.close()
+    println("Completed reading CSV")
+  }
+
 
 }
--- postgres-jdbc-access/src/main/scala/com/newhopebootcamps/util/ReadCsvUtil.scala
+++ postgres-jdbc-access/src/main/scala/com/newhopebootcamps/util/ReadCsvUtil.scala
@@ -1,42 +1,109 @@
 package com.newhopebootcamps.util
 
 import java.io._
+import java.sql.ResultSet
 import java.util.NoSuchElementException
 import scala.io.Source
-// import com.github.tototoshi.csv.CSVReader
 import com.github.tototoshi.csv._
+import com.newhopebootcamps.util.CommonUtils.isEmpty
 
 object ReadCsvUtil {
-  def read(): Unit = {
+  def read(filePath: String): Unit = {
     println("Reading CSV")
-  
-	// val reader = CSVReader.open(new File("/var/data/sample.csv"))
-	val reader = CSVReader.open(new File("sample.csv"))
+    val reader = CSVReader.open(new File(filePath))
     reader.readNext()
-
     // reader.all()
     reader.foreach(fields => println(fields))
 
     reader.close()
     println("Completed reading CSV")
-
   }
 
-  def write(): Unit = {
-    println("Writing CSV")
-   /* val resultSet2: java.sql.ResultSet = statement.executeQuery(query)
-    val csvWriter = new CSVWriter(new FileWriter("Output.csv"), ',')
-    csvWriter.writeAll(resultSet2, true)
-    csvWriter.close()*/
+  def write(filePath:String): Unit = {
+    println("Writing CSV ...... ")
 
-    //val writer = CSVWriter.open(new FileWriter("g:/School/data/test.csv"))
-	val writer = CSVWriter.open(new FileWriter("test.csv"))
-	
+    val writer = CSVWriter.open(new FileWriter(filePath))
     writer.writeAll(List(List("a", "b", "c"), List("d", "e", "f")))
-/*
-    using(CSVWriter.open(new FileWriter("test.csv"))) { writer =>
-      writer.writeAll(List(List("a", "b", "c"), List("d", "e", "f")))
-    }
-*/
   }
+
+  def writeAll(rs: ResultSet, filePath: String, isWriteHeader: Boolean = true): Unit = {
+    val demoCsvWriter = CSVWriter.open(new FileWriter(filePath))
+    var rl: List[List[String]] = List.empty
+    val columnNames = getColumnNames(rs)
+
+    if (isWriteHeader) {
+      rl :+= columnNames
+    }
+
+    rl ++= extractRecords(rs)
+    demoCsvWriter.writeAll(rl)
+  }
+
+  def getColumnNames(rs: ResultSet): List[String] = {
+    var columnList: List[String] = List.empty[String]
+    val metaData = rs.getMetaData
+    val columnCount = metaData.getColumnCount
+
+    val cols = (1 to columnCount).map { k =>
+      val columnName = metaData.getColumnLabel(k)
+      columnList :+= columnName
+    }
+
+    columnList
+  }
+
+  def extractRecords(rs: ResultSet): List[List[String]] = {
+    var dataList: List[List[String]] = List.empty
+    val columnNames = getColumnNames(rs)
+    while (rs.next) {
+      var columnList: List[String] = List.empty[String]
+      for (name <- columnNames) {
+        val value = rs.getString(name)
+        columnList :+= value
+      }
+
+      dataList :+= columnList
+    }
+
+    dataList
+  }
+
+  def rsWrite2(rs: ResultSet, headerString: String, filePath: String): Unit = {
+    val csvWriter = CSVWriter.open(new FileWriter(filePath))
+    var rl: List[List[String]] = List.empty
+
+    if (!isEmpty(headerString)) {
+      val header = headerString.split(",").toList
+      rl :+= header
+      println("CSV Header: ")
+      header.foreach(println)
+    }
+    else
+      println("CSV no header")
+
+    while (rs.next) {
+      var rec: List[String] = List.empty[String]
+      val countryName = rs.getString("code")
+      val location = rs.getString("region")
+
+      rec :+= countryName
+      rec :+= location
+
+      rl :+= rec
+    }
+
+    csvWriter.writeAll(rl)
+  }
+
+  @scala.annotation.tailrec
+  def getResult(resultSet: ResultSet, list: List[String] = Nil): List[String] = {
+    if (resultSet.next()) {
+      val value = resultSet.getString(1)
+      getResult(resultSet, value :: list)
+    }
+    else {
+      list
+    }
+  }
+
 }
