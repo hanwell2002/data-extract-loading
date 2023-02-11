d92c95ade26ff30fb700c2cdf09c3beb8ec5cccd
Hanwell <hanwell2002@gmail.com>
2023-02-11 11:00:54 -0500

    application configurations

--- application-config/src/main/scala/Main.scala
+++ application-config/src/main/scala/Main.scala
@@ -13,48 +13,42 @@ object Main {
   private val csvFile = "C:/var/output/resultset.csv"
 
   def main(args: Array[String]): Unit = {
-    extractAllDataToCsvDemo
 
-  //  loadResourceConfig
+    extractAllDataToCsvDemo
+    //  loadResourceConfig
 
     val envMap = sys.env
 
-    envMap.foreach{
-
-      pair=>
-        println("k = " + pair._1 + ":   value" + pair._2)
-
+    envMap.foreach {
+      pair =>
+        println("k = " + pair._1 + ".......value:" + pair._2)
     }
-
 
   }
 
   def loadResourceConfig() {
     val defaults = ConfigFactory.load()
-
     val conf = ConfigFactory.parseResources("database.conf").withFallback(defaults)
+
     val url = conf.getString("database.url")
     val username = conf.getString("database.username")
     val password = conf.getString("database.password")
     val host = conf.getString("database.host")
     println(s"url=$url, username=$username, pwd = $password, host= $host")
 
-
     val appName = conf.getString("appinfo.name")
-
     println(s"appName=$appName")
 
     println(conf.getString("Modules.Logging.logDb"))
     println(conf.getString("Modules.Tenants.tenantsDb"))
   }
 
-    def extractAllDataToCsvDemo() {
+  def extractAllDataToCsvDemo() {
     // classOf[org.postgresql.Driver]
     val defaults = ConfigFactory.load()
 
-      val customizeConfgi = defaults.getString("appinfo.config")
-
-    val file = new File(customizeConfgi)
+    val customizeConfig = defaults.getString("appinfo.config")
+    val file = new File(customizeConfig)
 
     val conf = ConfigFactory.parseFile(file).withFallback(defaults)
     val url = conf.getString("database.url")
@@ -69,40 +63,33 @@ object Main {
     println(conf.getString("Modules.Logging.logDb"))
     println(conf.getString("Modules.Tenants.tenantsDb"))
 
+    var connection: Connection = null
+    val query = "SELECT * from country"
 
-    /*
+    try {
+      connection = DriverManager.getConnection(url, username, password)
 
+      // create the statement, and run the query
+      import java.sql.ResultSet
+      val statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
+      val resultSet = statement.executeQuery(query)
 
-      var connection: Connection = null
-      val query = "SELECT * from country"
-
-      try {
-        connection = DriverManager.getConnection(url, username, password)
-
-        // create the statement, and run the query
-        import java.sql.ResultSet
-        val statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
-        val resultSet = statement.executeQuery(query)
-
-        while (resultSet.next) {
-             val countryName = resultSet.getString("code")
-             val location = resultSet.getString("region")
-             println("country-name = %s, location = %s".format(countryName, location))
-           }
-
-        println("###################################################")
-
-       // CsvFileUtil.writeAll(resultSet, csvFile, true)
-
-        println("-----------------------completed  ------------------------")
-
-      } catch {
-            case e => e.printStackTrace
+      while (resultSet.next) {
+        val countryName = resultSet.getString("code")
+        val location = resultSet.getString("region")
+        println("country-name = %s, location = %s".format(countryName, location))
       }
-      connection.close()
 
-    */
+      println("###################################################")
 
+       CsvFileUtil.writeAll(resultSet, csvFile, true)
+
+      println("-----------------------completed  ------------------------")
+
+    } catch {
+      case e => e.printStackTrace
+    }
+    connection.close()
   }
 
 
